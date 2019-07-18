e3cd24631c5ff6ab945bd4833f1f06299de43d3d
truthconductor <truthconductor@gmail.com>
2019-07-19 00:11:39 +0900

    Update posts_controller.rb
    create new post method in controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,34 @@
 class PostsController < ApplicationController
+  
+  before_action(:move_to_index, except: [:index, :show])
+
   def index
+    @posts = Post.includes(:user).all().order("created_at DESC")
   end
+
+  def new
+    @post = Post.new(post_params)
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :article).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    #ログインしていない時indexアクションを強制的に実行する
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
