2b4ad6fd75874d4f70825d7c40e08c82dba642c6
truthconductor <truthconductor@gmail.com>
2019-07-18 23:16:51 +0900

    Add posts Table
    Add post model
    define post table column
    associate user and post

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_17_141457) do
+ActiveRecord::Schema.define(version: 2019_07_18_140612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "article", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "nickname", null: false
@@ -29,4 +38,5 @@ ActiveRecord::Schema.define(version: 2019_07_17_141457) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
