8a6665e258a6cc6e76e6cd550aaebc61a6f350fb
truthconductor <truthconductor@gmail.com>
2019-07-19 22:53:25 +0900

    add users route and controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  def show
+    user = User.find(params[:id])
+    @nickname = user.nickname
+    @posts = user.posts
+              .order("created_at DESC")
+              .page(params[:page]).per(3)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
-  
+
+  #投稿
   resources :posts
+  #ユーザー
+  resources :users, only: [:show]
 end
